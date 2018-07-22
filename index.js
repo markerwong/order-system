ef9c6ee4b8e516271df18f4930f129f95da5b735
Marker Wong <markerwongdeveloper@gmail.com>
2018-07-22 16:11:30 +0800

    Setup environment with expressJS
    - Start index with expressJs
    - Setup lint test
    - Setup folders
    - Include config file

---
+++ index.js
@@ -0,0 +1,10 @@
+const config = require('config');
+
+
+const app = require('./src/app');
+
+const PORT = process.env.PORT || config.get('port');
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Started');
+});
+
+module.exports = app;
